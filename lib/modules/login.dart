d664b0345768b1934fda7b40aebffdd10ba026ac
1shaker <salmazini@students.iugaza.edu.ps>
2022-05-08 01:32:51 +0300

    Iug_Flutter_Project

---
+++ lib/modules/login.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class login_secrrn extends StatefulWidget {
+  const login_secrrn({Key? key}) : super(key: key);
+
+  @override
+  State<login_secrrn> createState() => _login_secrrnState();
+}
+
+class _login_secrrnState extends State<login_secrrn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title:const Text('Welcome to Flutter Future'),
+      ),
+    );
+  }
+}
