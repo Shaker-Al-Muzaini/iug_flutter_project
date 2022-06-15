f8aacb23b6ddad1d825e2aa65ba7b188bea8c27c
Shaker Al Muzaini <92122042+1shaker@users.noreply.github.com>
2022-06-15 13:45:30 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,7 @@ class  MyApp extends StatelessWidget{
                 subtitle1:TextStyle(
                   color:Colors.white,
                ),
-       
+      
               ),
               iconTheme:const IconThemeData(
                   color:Colors.white
