5dfcaa1528df475ed15519ec8569ec5b325d1d7a
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2022-07-18 16:58:27 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: unused_import, unused_local_variable, must_be_immutable, use_key_in_widget_constructors
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart'; 
-import 'package:flutter_bloc/flutter_bloc.dart'; 
+import 'package:flutter_bloc/flutter_bloc.dart';  
 import 'package:hexcolor/hexcolor.dart';
 import 'package:iug_flutter_project/shared/network/cache_helper.dart';
 import 'package:flutter/material.dart';
